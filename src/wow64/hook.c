3ea31f3dac9c2d43827e216a540709fa46cc512f
TheDeadFish <michaelstamper1@gmail.com>
2019-06-21 00:51:20 +0000

    1. wow64: implemented ntdllex loading when OSMajorVersion >= 6

--- src/wow64/hook.c
+++ src/wow64/hook.c
@@ -1,4 +1,5 @@
 #include <ntndk.h>
+#include "stdshit.h"
 
 /*
 NTSTATUS NTAPI RtlAppendUnicodeToString(
@@ -6,11 +7,24 @@ NTSTATUS NTAPI RtlAppendUnicodeToString(
   PCWSTR          Source
 ); */
 
+static
+BOOL IsNt6Process(void) 
+{
+	return NtCurrentPeb()->OSMajorVersion >= 6;
+}
+
 NTSTATUS NTAPI RtlAppendUnicodeToString_hook(
   PUNICODE_STRING Destination,
   PCWSTR          Source
 ) {
-	
-	return RtlAppendUnicodeToString(
-		Destination, L"krnlex\\ntdll.dll");
+	if(IsNt6Process()) Source = L"\\krnlex\\ntdll.dll";
+	return RtlAppendUnicodeToString(Destination, Source);
+}
+
+__attribute__ ((dllimport))
+NTSTATUS NTAPI LdrGetKnownDllSectionHandle(void* a, void* b, void* c);
+NTSTATUS NTAPI LdrGetKnownDllSectionHandle_hook(void* a, void* b, void* c)
+{
+	if(IsNt6Process()) return STATUS_UNSUCCESSFUL;
+	return LdrGetKnownDllSectionHandle(a, b, c);
 }
