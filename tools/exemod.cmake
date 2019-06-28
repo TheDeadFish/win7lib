340fd4d9af79b471ddba1d28d02284da7bc2b015
TheDeadFish <michaelstamper1@gmail.com>
2019-06-28 21:59:08 +0000

    1. add_exm_file: improved api

--- tools/exemod.cmake
+++ tools/exemod.cmake
@@ -6,7 +6,7 @@ else()
 	set (ARCH32 "32")
 endif()
 
-function (add_exm_file name arch)
+function (add_exm_file arch)
 	if(arch MATCHES "^x64")
 		if(NOT ARCH STREQUAL "amd64")
 			return()
@@ -17,22 +17,34 @@ function (add_exm_file name arch)
 		endif()
 	endif()
 
-	set (exm_file "${EXM_DIR}/${name},extxp${ARCH32}.exm")
-	if (NOT exm_cmd)
-		set(exm_out ${exm_file} PARENT_SCOPE)
-	else()
-		set(exm_file "+${exm_file}")
+	if (exm_cmd)
+		set(PLUS "+")
 	endif()
 	
 	set (deps ${ARGN})
 	list(FILTER deps EXCLUDE REGEX ^-)
 	set(exm_cmd ${exm_cmd} DEPENDS ${deps} COMMAND 
-		exe_mod ${exm_file}:${arch} ${ARGN} PARENT_SCOPE)
+		exe_mod ${PLUS}EXM_FILE:${arch} ${ARGN} PARENT_SCOPE)
 
 endfunction()
 
 function (add_exm_target name)
-	add_custom_command(OUTPUT ${exm_out}
+	if(NOT exm_cmd)
+		return()
+	endif()
+
+	set (grp ${ARGN})
+	if (NOT grp)
+		set(grp "extxp")
+	endif()
+	
+	set (exm_file "${EXM_DIR}/${name},${grp}${ARCH32}.exm")
+	string(REPLACE "EXM_FILE" ${exm_file} exm_cmd "${exm_cmd}")
+	add_custom_command(OUTPUT ${exm_file}
 		${exm_cmd} COMMAND_EXPAND_LISTS)
-	add_custom_target(exm_${name} ALL DEPENDS ${exm_out})	
+	
+	string(REPLACE "," "_" target "${name},${grp}")
+	add_custom_target(${target} ALL DEPENDS ${exm_file})
+	set(exm_cmd "" PARENT_SCOPE)
+	
 endfunction()
